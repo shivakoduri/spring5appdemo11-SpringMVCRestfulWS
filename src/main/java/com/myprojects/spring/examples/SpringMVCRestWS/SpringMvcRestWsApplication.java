9cda997474aa7d79fd9eaa729f3a28b1fd01826f
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-06 20:57:56 +0530

    initial commit

---
+++ src/main/java/com/myprojects/spring/examples/SpringMVCRestWS/SpringMvcRestWsApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.spring.examples.SpringMVCRestWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRestWsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myprojects/spring/examples/SpringMVCRestWS/SpringMvcRestWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myprojects.spring.examples.SpringMVCRestWS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcRestWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
